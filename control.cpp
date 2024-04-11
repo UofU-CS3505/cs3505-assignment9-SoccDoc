a2e6016cd18e1a04843b209c6f6a81165194e802
Ghskljd <ryannip86@gmail.com>
2024-04-11 12:50:26 -0600

    added drawing functionality

--- control.cpp
+++ control.cpp
@@ -1,4 +1,72 @@
 #include "control.h"
 #include <QWidget>
+#include <QPainter>
+#include <QMouseEvent>
+#include <QDebug>
 
-Control::Control(QWidget *parent) {}
+Control::Control(QWidget *parent) : QWidget(parent) {
+    setAttribute(Qt::WA_StaticContents);
+}
+
+void Control::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    QRect dirtyRect = event->rect();
+    painter.drawImage(dirtyRect, image, dirtyRect);
+}
+
+void Control::resizeEvent(QResizeEvent *event)
+{
+    if (width() > image.width() || height() > image.height()) {
+        resizeImage(&image, QSize(width(), height()));
+        update();
+    }
+    QWidget::resizeEvent(event);
+}
+
+void Control::resizeImage(QImage *image, const QSize &newSize)
+{
+    if (image->size() == newSize)
+        return;
+
+    QImage newImage(newSize, QImage::Format_RGB32);
+    newImage.fill(qRgb(255, 255, 255));
+    QPainter painter(&newImage);
+    painter.drawImage(QPoint(0, 0), *image);
+    *image = newImage;
+}
+
+void Control::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+        lastPoint = event->position().toPoint();
+        scribbling = true;
+    }
+}
+
+void Control::mouseMoveEvent(QMouseEvent *event)
+{
+    if ((event->buttons() & Qt::LeftButton) && scribbling)
+        drawLineTo(event->position().toPoint());
+}
+
+void Control::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton && scribbling) {
+        drawLineTo(event->position().toPoint());
+        scribbling = false;
+    }
+}
+
+void Control::drawLineTo(const QPoint &endPoint)
+{
+    QPainter painter(&image);
+    painter.setPen(QPen(Qt::blue, 10, Qt::SolidLine, Qt::RoundCap,
+                        Qt::RoundJoin));
+    painter.drawLine(lastPoint, endPoint);
+
+    int rad = (10 / 2) + 2;
+    update(QRect(lastPoint, endPoint).normalized()
+               .adjusted(-rad, -rad, +rad, +rad));
+    lastPoint = endPoint;
+}
